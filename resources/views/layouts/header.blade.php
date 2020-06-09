99392122f9276e0d319f8552dfadc32693bba34e
MNGOM <mngom@24eme.fr>
2020-06-09 12:01:20 +0200

    Organisation : Up to date

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,7 +7,7 @@
      </div>
      <ul class="nav navbar-nav">
        <li class="nav-item active"><a class="nav-link" href="#">Fil d'actualités</a></li>
-       <li class="nav-item"><a class="nav-link" href="#">Réserver une salle</a></li>
+       <li class="nav-item"><a class="nav-link" href="reserver">Réserver une salle</a></li>
        <li class="nav-item"><a class="nav-link" href="#">Trombinoscope</a></li>
      </ul>
      <ul class="nav navbar-nav navbar-right">
