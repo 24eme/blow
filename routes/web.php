6e6b5575501b88ed9a72b1766e019f941c947417
MNGOM <mngom@24eme.fr>
2020-06-12 14:12:14 +0200

    Updated

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,9 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 Route::get('/', function () {
     return view('index');
@@ -17,11 +15,15 @@ Route::get('/', function () {
 Route::get('/reserver', function() {
         return view('frontend/reserver');
 });
+Route::get('/trombinoscope', function() {
+        return view('frontend/trombinoscope');
+});
 Route::get('/aide', function() {
         return view('frontend/aide');
 });
 Route::post('/addEvent', 'addEvent@InsertEvent');
 
+//Honorine
 Route::get('/', 'EvenementController@list');
 Route::get('json-list-events', 'EvenementController@listTest'); //pour les evenements
 Route::get('json-list-resources', 'SalleController@listTest'); //pour les resources
