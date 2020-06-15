7e3cd4a53653f75f98ce7e7442236e06f9c6bfd4
MNGOM <mngom@24eme.fr>
2020-06-15 15:06:57 +0200

    Up to date

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+
+<div class="background">
+
+<div class="container">
+        <div class="page" id="p1">
+               <section><span class="title">Bolt</span>
+               </section>
+        </div>
+        <div class="page" id="p2">
+              <section><div id="calendar" data-toggle="modal" data-target="#reservation-modal"></div></section>
+        </div>
+</div>
+
+
+</div>
+
+
+
+
+
+@endsection
