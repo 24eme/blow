0d629174586f0b2431ca39cf91a49dfe4a34d2ca
Honorine Marie Christy <honorine@24eme.fr>
2020-06-16 11:46:19 +0200

    seed mis  jour avec la table rooms

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,15 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION C3","eventColor"=>"#a2cbf5"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION TA34","eventColor"=>"#8a9482"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION","eventColor"=>"#e3b2a1"]);
-      DB::table("salles")->insert(["title"=>"SALLE DES FETES 203","eventColor"=>"#fac800"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION 459","eventColor"=>"#f5e4d5"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION 345","eventColor"=>"#ff5ea4"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION 975","eventColor"=>"#ff672b"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION 325","eventColor"=>"#88f77c"]);
-      DB::table("salles")->insert(["title"=>"SALLE DE REUNION 342","eventColor"=>"#e2bfff"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION C3","eventColor"=>"#a2cbf5"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION TA34","eventColor"=>"#8a9482"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION","eventColor"=>"#e3b2a1"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DES FETES 203","eventColor"=>"#fac800"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION 459","eventColor"=>"#f5e4d5"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION 345","eventColor"=>"#ff5ea4"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION 975","eventColor"=>"#ff672b"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION 325","eventColor"=>"#88f77c"]);
+      DB::table("rooms")->insert(["title"=>"SALLE DE REUNION 342","eventColor"=>"#e2bfff"]);
         // $this->call(UserSeeder::class);
     }
 }
