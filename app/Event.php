1c5efa28e2588a23e86d871344baa0e303499b8e
Sidi Sokhona <ssokhona@24eme.fr>
2020-06-16 15:45:05 +0200

    changement des noms pour les events dans le controller la migration et model

--- app/Event.php
+++ app/Event.php
@@ -2,38 +2,19 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class Events extends
+class Event extends Model
 {
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
+  protected $guarded = [];  //à changer pour le sécurité demandez à Sidi
+  protected $dates = [
+        'start',
+        'end',
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  protected $dateFormat = 'Y-m-dTH:i:sZ';
+  public function events()
+  {
+    return $this->belongsTo('App\Room');
+  }
 }
