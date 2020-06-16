bf99d99175d42eb1f06a24b11aec6bdd3f902102
MNGOM <mngom@24eme.fr>
2020-06-16 12:32:31 +0200

    Update

--- resources/views/frontend/components/modal.blade.php
+++ resources/views/frontend/components/modal.blade.php
@@ -34,60 +34,10 @@
             avant </label>
             <br></br>
             <div class="btn-wrapper">
+              <button type="submit" class="btn btn-secondary btn-modal" data-dismiss="modal">Modifier</button>
                 <button type="submit" class="btn btn-secondary btn-modal" data-dismiss="modal">Annuler</button>
                 <button type="submit" class="btn btn-primary btn-modal">Confirmer</button>
             </div>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <div class="note"><span><p><strong>Note :</strong> Votre réservation ne sera effective qu'après validation de votre gestionnaire d'espace, vous serez alors notifié par mail.</p></span></div>
-        </div>
-
-      </div>
-    </div>
-</div>
-
-<!-- end inseet -->
-
-<div class="modal fade" id="update-modal" tabindex="-1" role="dialog" aria-labelledby="Reservation" aria-hidden="true">
-    <div class="modal-dialog " role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <div class="header-box"><h5 class="modal-title">Réserver votre salle</h5>
-            <label>Organisateur : { $nomutilisateurousociete}}</label><br>
-            <label>Nom de salle : </label><label id="nomdesalle"></label><br>
-            <label>Surface : { $nomdesalle}}</label><label id="capacite"></label><br>
-            <label>Capacité : { $capacity}}</label>
-
-          </div>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form method="POST" action="{{url('/updateEvent/{id}')}}">
-            <input type="text" id="title" name="title" placeholder="Nom de l'événement" value=""><br></br>
-      <!-- A faire droit de réserver que 3h de suite-->
-            <input name="resourceId" id="resourceId" style="display:none">
-            <label>Début : </label>
-            <input type="date" id="start" name="datededebut" value="2020-06-09T09:30:00Z"><input type="time" id="startTime" name="startTime" min="06:00" max="20:00" required><br>
-            <hr>
-            <label>Fin : </label><input type="date" id="end" name="end" value=""><input type="time" id="endTime" name="endTime" min="06:00" max="20:00" required><br>
-            <hr>
-            <input type="checkbox" name="rappel">
-            <label>Planifier un rappel
-              <select>
-                  <option>une heure</option>
-                  <option>un jour</option>
-                  <option>un mois</option>
-              </select>
-            avant </label>
-            <br></br>
-            <div class="btn-wrapper">
-                <button type="submit" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                <button type="submit" class="btn btn-primary">Confirmer</button>
-            </div>
-            {{ csrf_field() }}
           </form>
         </div>
         <div class="modal-footer">
