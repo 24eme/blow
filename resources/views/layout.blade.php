5f00de59b499f5194f7f418486e4c67deb97c174
MNGOM <mngom@24eme.fr>
2020-06-16 11:21:42 +0200

    Résolution problème index

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
  <head>
-    @include('layouts.head')
-    @include('frontend.components.calendar')
+    @include('layouts.head') 
  </head>
  <body>
     @include('layouts.header')
-      @yield('content') 
+      @yield('content')
     @include('layouts.footer')
     @include('layouts.footer-scripts')
  </body>
