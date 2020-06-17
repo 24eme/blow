b96821b1e7dcff0ec017670d59eea9fd6001b5ae
MNGOM <mngom@24eme.fr>
2020-06-17 19:58:34 +0200

    Update

--- resources/views/frontend/components/modal.blade.php
+++ resources/views/frontend/components/modal.blade.php
@@ -3,17 +3,19 @@
       <div class="modal-content">
         <div class="modal-header">
           <div class="header-box"><h5 class="modal-title">Réserver votre salle</h5>
+
             <label>Organisateur : { $nomutilisateurousociete}}</label><br>
-            <label>Nom de salle : </label><label id="room_name"></label><br>
-            <label>Surface : { $nomdesalle}}</label><label id="surface"></label><br>
+            <label>Nom de salle : </label><label id="room_name"></label><br> 
             <label>Capacité : { $capacity}}</label><label id="capacity"></label><br>
 
           </div>
+          <button type="button" class="close"><i class="fas fa-share-alt"></i></button>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
+
           <form method="POST" action="{{url('addEvent')}}">
             <input type="text" id="event_name" name="event_name" placeholder="Nom de l'événement" value=""><br></br>
           <input type="text" id="event_id" name="event_id" value="" style="display:none">
@@ -26,11 +28,6 @@
             <br></br>
             <div class="btn-wrapper">
 
-                @if ( $post->getAuthor()->getId() === Auth::user()->getId() )
-                echo '<button type="submit" class="btn btn-secondary btn-modal" data-dismiss="modal">Modifier</button>
-                <button type="submit" class="btn btn-secondary btn-modal" data-dismiss="modal">Supprimer</button>'
-
-                @endif
 
                 <button type="submit" class="btn btn-secondary btn-modal" data-dismiss="modal">Annuler</button>
                 <button type="submit" class="btn btn-primary btn-modal">Confirmer</button>
