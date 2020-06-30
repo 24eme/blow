5b831b844a7382013261125775c7595fc4e533a9
Honorine Marie Christy <honorine@24eme.fr>
2020-06-30 14:28:01 +0200

    fonction update pour une salle

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -13,4 +13,26 @@ class RoomController extends Controller
       return $rooms->toJson();
   }
 
+  public function update(){
+    $roomId = $request->input('room_id');
+
+    if($roomId!=null){
+
+       $title = $request->input('room_name');
+       $equipment=$request->input('equipment');
+       $capacity=$request->input('capacity');
+       $eventColor=$request->input('eventColor');
+
+       $data = array('title'=>$title,'equipement'=>$equipment,'capacity'=>$capacity,'eventColor'=>$eventColor);
+       $query =  DB::table('rooms')
+       ->where('id', $roomId)
+       ->update($data);
+
+        return redirect()->to(url()->previous() . '#reserver')->with('failUnavailable',
+                'Votre événement a été modifié');
+}
+       return redirect()->to(url()->previous() . '#reserver')->with('failUnavailable',
+             'On ne peut pas modifié un événement inexistant');
+  }
+
 }
