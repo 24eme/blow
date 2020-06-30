8e8c4e9f4b4cc7ec6f708aabe63c78657f084c6d
Honorine Marie Christy <honorine@24eme.fr>
2020-06-30 16:05:07 +0200

    datepicker en français

--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -5,6 +5,17 @@
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
 $( function() {
-  $( "#datepicker" ).datepicker();
-} );
+$( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi" ],
+      dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa" ],
+      dayNamesShort:[ "Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam" ],
+      gotoCurrent: true,
+      monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre" ],
+      monthNamesShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Juin", "Juil", "Aoû", "Sep", "Oct", "Nov", "Dec" ],
+      prevText: "Préc",
+      nextText: "Suiv",
+    });
+      });
 </script>
