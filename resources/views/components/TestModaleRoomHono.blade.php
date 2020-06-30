88954403956b691b341d032d87e7f22a1e1d2378
Honorine Marie Christy <honorine@24eme.fr>
2020-06-30 14:28:40 +0200

    champs pour écrire l'id de la salle à modifier

--- resources/views/components/TestModaleRoomHono.blade.php
+++ resources/views/components/TestModaleRoomHono.blade.php
@@ -3,15 +3,14 @@
       <div class="modal-content">
         <div class="modal-body">
           <form method="PUT" id="methode" action="{{url('updateRooms')}}">
+          <input type='text' id='room_id' name='room_id' placeholder="id de la salle à modifié" value=""><br></br>
           <input type="text" id="room_name" name="room_name" placeholder="Nom de la salle" value="" required><br></br>
           <input type="text" id="equipment" name="equipment" placeholder="Equipements"value=""><br></br>
           <input type="text" id="capacity" name="capacity" placeholder="Capcity"value=""><br></br>
           <input type="text" id="eventColor" name="eventColor" placeholder="Couleur des évenements"value="">
           <br></br>
             <div class="btn-wrapper">
-
               <input type="submit" class="btn btn-primary btn-modal" name="action" value="Update">
-
             </div>
             {{ csrf_field() }}
           </form>
