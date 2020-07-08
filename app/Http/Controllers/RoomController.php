c75f8d1830251b5f12618b40c88b67d02831bf2e
Sidi Sokhona <ssokhona@24eme.fr>
2020-07-08 14:45:06 +0200

    création des methodes dans le model Room et appel des mthodes dans le controller

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -11,42 +11,54 @@ use App\Room;
 
 class RoomController extends Controller{
 
+  protected $room;
+
+
+  public function __construct() {
+
+    $this->room = new Room;
+
+
+  }
+
   public function show(){
-      $rooms = Room::all();
-      return $rooms->toJson();
+
+      return $this->room->showRoom();
   }
 
   public function create(Request $request){
-        $request->validate([
-        'room_name' => 'required',
-        'capacity' => 'integer',  //ajouter la validation de l'image
-        ]);
+    $request->validate([
+    'room_name' => 'required',
+    'capacity' => 'integer',  //ajouter la validation de l'image
+    ]);
+    // Mame : essai réussi insertion d'image
+    // $image = $request->file('image')->store('images');
+    // Storage::disk('public')->put($image, 'Contents');
+   // $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
+   // $destinationPath = public_path('/thumbnail');
+   // $img = Image::make($image->getRealPath());
+   // $img->resize(100, 100, function ($constraint) {
+   //     $constraint->aspectRatio();
+   // })->save($destinationPath.'/'.$input['imagename']);
 
-       $room = new Room;
-       $room->title = $request->room_name;
-       $room->equipment = $request->equipment;
-       $room->capacity = $request->capacity;
-       $room->eventColor = $request->eventColor;
-       $room->image=$request->image;
-       $room->save();
+   // /*After Resize Add this Code to Upload Image*/
+   // $destinationPath = public_path('/');
+   // $image->move($destinationPath, $input['imagename']);
 
-      // Mame : essai réussi insertion d'image
-      // $image = $request->file('image')->store('images');
-      // Storage::disk('public')->put($image, 'Contents');
-     // $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
-     // $destinationPath = public_path('/thumbnail');
-     // $img = Image::make($image->getRealPath());
-     // $img->resize(100, 100, function ($constraint) {
-     //     $constraint->aspectRatio();
-     // })->save($destinationPath.'/'.$input['imagename']);
+   $name=$request->room_name;
+   $equipment=$request->equipment;
+   $capacity=$request->capacity;
+   $eventColor= $request->eventColor;
+   $image=$request->image;
 
-     // /*After Resize Add this Code to Upload Image*/
-     // $destinationPath = public_path('/');
-     // $image->move($destinationPath, $input['imagename']);
-      return redirect()->back()->with('success', 'La salle a bien été ajouté');
+   $this->room->saveRoom($name,$equipment,$capacity,$eventColor,$image);
+
+    return redirect()->back()->with('success', 'La salle a bien été ajouté');
+
   }
 
   public function update(Request $request){
+
 
     $roomId = $request->room_id;
 
@@ -65,11 +77,14 @@ class RoomController extends Controller{
     }
 
       return redirect()->back()->with('error', 'La salle n\'a pas été identifée');
-
   }
-  public function delete(Request $request,$id){
-         $room = Room::find($id)->delete();
-             return redirect()->back()->with('success', 'Votre salle a bien été supprimé');
+
+
+
+  public function delete(Request $request, $id){
+
+    $room = Room::find($id)->delete();
+        return redirect()->back()->with('success', 'Votre salle a bien été supprimé');
    }
 
 }
--- app/Room.php
+++ app/Room.php
@@ -3,16 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Room extends Model
 {
     //
     protected $fillable = ['equipment'];
-    
+
     public function events()
     {
    return $this->hasMany('App\Event');
     }
+
+    public function showRoom(){
+    $rooms = Room::all();
+    return $rooms->toJson();
+    }
+
+    public function saveRoom($name,$equipment,$capacity,$eventColor,$image){
+      $room = new Room;
+      $room->title = $name;
+      $room->equipment = $equipment;
+      $room->capacity = $capacity;
+      $room->eventColor = $eventColor;
+      $room->image=$image;
+      $room->save();
+    }
+
+    
+
+
 
 
 
