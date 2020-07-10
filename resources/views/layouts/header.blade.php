d3e147b659009a26eed6b7613d6172cbe0efa408
Honorine Marie Christy <honorine@24eme.fr>
2020-07-10 12:32:58 +0200

    vue head avec un calendrier pour un admin

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -28,21 +28,16 @@
        <span class="navbar-toggler-icon"></span>
    </button>
    <div class="collapse navbar-collapse" id="navbarMenuAd">
-   <!-- <ul class="nav navbar-nav navbar-right"> -->
       <ul class="nav navbar-nav m-auto">
-     <li class="nav-item"><a class="custom-link" href="#"><i class="fas fa-user-circle"></i>{{ Auth::user()->name }}</a></li>
+     <li class="nav-item"><i class="fas fa-user-circle"></i>{{ Auth::user()->name }}</a></li>
+
      @if(Auth::check())
              @if (Auth::user()->isAdmin())
-             <li class="nav-item dropdown"><a class="custom-link dropdown-toggle" data-toggle="dropdown" href="/admin"><i class="fas fa-chart-line"></i>Tableau de bord</a>
-             <ul class="dropdown-menu">
-                 <li class="dropdown-item"><a class="custom-link" href="/admin" onclick="openTab(event, 'user-tab')"><i class="fas fa-users"></i></i>Utilisateurs</a></li>
-                 <li class="dropdown-item"><a class="custom-link" href="/admin" onclick="openTab(event, 'room-tab')"><i class="fas fa-list"></i>Salles</a></li>
-                 <li class="dropdown-item"><a class="custom-link" href="/admin" onclick="openTab(event, 'event-tab')"><i class="far fa-calendar-check"></i>Evenements</a></li>
-                 <li class="dropdown-item"><a class="custom-link" href="/admin" onclick="openTab(event, 'calendar-tab')"><i class="far fa-calendar-alt"></i>Calendrier</a></li>
-             </ul>
-             </li>
+                  <li class="nav-item"><a class="custom-link" href="/home"><i class="fas fa-calendar-alt"></i>Calendrier</a></li>
+             <li class="nav-item dropdown"><a class="custom-link" href="/admin"><i class="fas fa-chart-line"></i>Tableau de bord</a></li>
+                        @endif
              <li class="nav-item"><a class="custom-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>{{ __('Déconnexion') }}</a></li><form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form></li>
-             @endif
+
      @endif
    </ul>
   </div>
