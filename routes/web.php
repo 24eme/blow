54afe173fec1a67bd6a41d2a56db1379ddc97b3f
Honorine Marie Christy <honorine@24eme.fr>
2020-07-10 17:43:04 +0200

    derniere version

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home')->middleware('verified');
+    return view('home');
 });
 Route::get('/showEvents', 'EventController@show');
 Route::post('/createEvent','EventController@create')->name('createEvent');
